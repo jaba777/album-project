ce84f4e812b7fe7fa954ac78173d2fbd1004e857
jaba777 <tskhovreba777@gmail.com>
2022-02-06 02:50:19 +0400

    build html

---
+++ app.js
@@ -0,0 +1,5 @@
+const BtnIcons=document.querySelector('.button-icons');
+const Menu =document.querySelector('.Menu');
+BtnIcons.addEventListener('click',function(){
+    Menu.classList.toggle('active');
+})
